52c4a31a8a61f09770c5ddd682b65b84a5161372
Benjamin Rupp <uwecl@student.kit.edu>
2016-11-09 15:42:29 +0100

    Improve java doc
    The helper class is only used for the reactions. A global helper class
    exists in the '.global' package which can be used in both, the
    reflections and the test cases.

--- tools.vitruv.applications.asemsysml.sysml2asem/src/tools/vitruv/applications/asemsysml/sysml2asem/util/ASEMSysMLReactionHelper.java
+++ tools.vitruv.applications.asemsysml.sysml2asem/src/tools/vitruv/applications/asemsysml/sysml2asem/util/ASEMSysMLReactionHelper.java
@@ -6,28 +6,26 @@ import static tools.vitruv.applications.asemsysml.sysml2asem.global.ASEMSysMLCon
 import tools.vitruv.applications.asemsysml.sysml2asem.global.ASEMSysMLHelper;
 
 /**
- * A helper class containing methods which are useful for both transformation
- * directions. Both for the reactions and for the test implementation.
+ * A helper class containing methods which are useful for the transformation using reactions.
  * 
  * @author Benjamin Rupp
  */
 public class ASEMSysMLReactionHelper {
 
-	/**
-	 * Get the project model path for the ASEM model which corresponds to a
-	 * SysML Block.
-	 * 
-	 * @param blockName
-	 *            Name of the corresponding SysML block.
-	 * @return Project model path of the ASEM model.
-	 */
-	public static String getASEMProjectModelPath(final String blockName) {
+    /**
+     * Get the project model path for the ASEM model which corresponds to a SysML Block.
+     * 
+     * @param blockName
+     *            Name of the corresponding SysML block.
+     * @return Project model path of the ASEM model.
+     */
+    public static String getASEMProjectModelPath(final String blockName) {
 
-		String modelName = getASEMModelName(blockName);
-		String projectModelPath = ASEMSysMLHelper.getProjectModelPath(modelName, ASEM_FILE_EXTENSION);
+        String modelName = getASEMModelName(blockName);
+        String projectModelPath = ASEMSysMLHelper.getProjectModelPath(modelName, ASEM_FILE_EXTENSION);
 
-		return projectModelPath;
+        return projectModelPath;
 
-	}
+    }
 
 }
