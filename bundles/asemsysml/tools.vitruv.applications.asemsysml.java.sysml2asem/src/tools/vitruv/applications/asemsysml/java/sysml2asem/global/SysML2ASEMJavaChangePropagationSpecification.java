73808db6f7403c62c5cd9d9f31abd5b91c63cc6a
Benjamin Rupp <uwecl@student.kit.edu>
2017-01-17 13:59:27 +0100

    Replace java change propagation specification with java executor

--- bundles/asemsysml/tools.vitruv.applications.asemsysml.java.sysml2asem/src/tools/vitruv/applications/asemsysml/java/sysml2asem/global/SysML2ASEMJavaChangePropagationSpecification.java
+++ bundles/asemsysml/tools.vitruv.applications.asemsysml.java.sysml2asem/src/tools/vitruv/applications/asemsysml/java/sysml2asem/global/SysML2ASEMJavaChangePropagationSpecification.java
@@ -1,50 +1,132 @@
 package tools.vitruv.applications.asemsysml.java.sysml2asem.global;
 
-import org.apache.log4j.Level;
-import org.apache.log4j.Logger;
+import java.util.HashSet;
+import java.util.Set;
+
 import org.eclipse.uml2.uml.UMLPackage;
 
 import edu.kit.ipd.sdq.ASEM.ASEMPackage;
-import tools.vitruv.applications.asemsysml.java.sysml2asem.SysML2ASEMJavaExecutor;
-import tools.vitruv.framework.change.processing.impl.CompositeChangePropagationSpecification;
+import tools.vitruv.applications.asemsysml.java.sysml2asem.transformations.BlockTransformation;
+import tools.vitruv.applications.asemsysml.java.sysml2asem.transformations.PortTransformation;
+import tools.vitruv.applications.asemsysml.java.sysml2asem.transformations.PortTypeTransformation;
+import tools.vitruv.applications.asemsysml.java.sysml2asem.transformations.PrimitiveTypeTransformation;
+import tools.vitruv.applications.asemsysml.java.sysml2asem.transformations.DebugTransformation;
+import tools.vitruv.applications.asemsysml.java.sysml2asem.transformations.PartTransformation;
+import tools.vitruv.applications.asemsysml.java.sysml2asem.transformations.PortDirectionTransformation;
+import tools.vitruv.applications.asemsysml.java.sysml2asem.JavaTransformationRealization;
+import tools.vitruv.applications.asemsysml.java.sysml2asem.transformations.BlockNameTransformation;
+import tools.vitruv.applications.asemsysml.java.sysml2asem.util.Change2TransformationMap;
+import tools.vitruv.framework.change.echange.EChange;
+import tools.vitruv.framework.change.processing.impl.AbstractEChangePropagationSpecification;
+import tools.vitruv.framework.correspondence.CorrespondenceModel;
+import tools.vitruv.framework.userinteraction.UserInteracting;
 import tools.vitruv.framework.userinteraction.impl.UserInteractor;
+import tools.vitruv.framework.util.command.ChangePropagationResult;
 import tools.vitruv.framework.util.datatypes.MetamodelPair;
 
 /**
  * Change propagation specification for the SysML2ASEM transformation using java transformations.
- * This class is responsible for the change handling and manages the main and preprocessors.
+ * This class is responsible for the mapping of a change and the java transformation which reacts to
+ * this change.
  * 
  * @author Benjamin Rupp
- *
+ * 
  */
-public class SysML2ASEMJavaChangePropagationSpecification extends CompositeChangePropagationSpecification {
+public class SysML2ASEMJavaChangePropagationSpecification extends AbstractEChangePropagationSpecification {
 
-    private static Logger logger = Logger.getLogger(SysML2ASEMJavaChangePropagationSpecification.class);
-
+    private Change2TransformationMap change2TransformationMap;
     private final MetamodelPair metamodelPair;
 
     /**
-     * Create new SysML2ASEM change propagation specification for the java transformations.
+     * Create a new change executor with the given user interactor.
+     *
      */
     public SysML2ASEMJavaChangePropagationSpecification() {
-
         super(new UserInteractor());
-
-        logger.setLevel(Level.INFO);
-
+        this.change2TransformationMap = new Change2TransformationMap();
         this.metamodelPair = new MetamodelPair(UMLPackage.eNS_URI, ASEMPackage.eNS_URI);
-        setup();
-
+        this.setup();
     }
 
     private void setup() {
-        logger.info("[ASEMSysML][Java] Set up transformation environment using java transformations.");
-        this.addChangeMainprocessor(new SysML2ASEMJavaExecutor(this.getUserInteracting()));
+
+        // Add the available java transformations.
+        this.addTransformation(new DebugTransformation(this.getUserInteracting()));
+        this.addTransformation(new BlockTransformation(this.getUserInteracting()));
+        this.addTransformation(new BlockNameTransformation(this.getUserInteracting()));
+        this.addTransformation(new PortTransformation(this.getUserInteracting()));
+        this.addTransformation(new PortDirectionTransformation(this.getUserInteracting()));
+        this.addTransformation(new PortTypeTransformation(this.getUserInteracting()));
+        this.addTransformation(new PrimitiveTypeTransformation(this.getUserInteracting()));
+        this.addTransformation(new PartTransformation(this.getUserInteracting()));
+
     }
 
     @Override
     public MetamodelPair getMetamodelPair() {
         return this.metamodelPair;
+    }
+
+    @Override
+    protected boolean doesHandleChange(EChange change, CorrespondenceModel correspondenceModel) {
+
+        final Set<JavaTransformationRealization> relevantTransformations = this.getRelevantTransformations(change);
+        final Boolean handleChange = !relevantTransformations.isEmpty();
+
+        return handleChange;
+    }
+
+    @Override
+    protected ChangePropagationResult propagateChange(EChange change, CorrespondenceModel correspondenceModel) {
+
+        final ChangePropagationResult propagationResult = new ChangePropagationResult();
+        final Set<JavaTransformationRealization> relevantTransformations = this.getRelevantTransformations(change);
+
+        ChangePropagationResult currentResult;
+
+        for (JavaTransformationRealization transformation : relevantTransformations) {
+
+            currentResult = transformation.applyChange(change, correspondenceModel);
+
+            // Add the result of each transformation to the overall propagation result.
+            propagationResult.integrateResult(currentResult);
+        }
+
+        return propagationResult;
+    }
+
+    @Override
+    public void setUserInteracting(UserInteracting userInteracting) {
+        /*
+         * If the user interacting has changed, set the new user interacting, clean the change to
+         * transformation map and initialize the java transformation classes again.
+         */
+        super.setUserInteracting(userInteracting);
+
+        this.change2TransformationMap = new Change2TransformationMap();
+        this.setup();
+    }
+
+    private Set<JavaTransformationRealization> getRelevantTransformations(final EChange change) {
+
+        Set<JavaTransformationRealization> relevantTransformations = new HashSet<JavaTransformationRealization>();
+        final Set<JavaTransformationRealization> transformations = this.change2TransformationMap
+                .getJavaTransformations(change);
+
+        for (JavaTransformationRealization transformation : transformations) {
+
+            if (transformation.doesHandleChange(change)) {
+                relevantTransformations.add(transformation);
+            }
+        }
+
+        return relevantTransformations;
+    }
+
+    private void addTransformation(final JavaTransformationRealization transformation) {
+
+        this.change2TransformationMap.addJavaTransformation(transformation.getExpectedChangeType(), transformation);
+
     }
 
 }
