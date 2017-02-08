bbc245caf512f0975a3486f76590f5ac1484dace
Benjamin Rupp <uwecl@student.kit.edu>
2017-02-08 16:59:16 +0100

    Add new project for ASEM to SysML transformation tests

---
+++ tests/asemsysml/tools.vitruv.applications.asemsysml.tests.asem2sysml/src/tools/vitruv/applications/asemsysml/tests/asem2sysml/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * This package contains all the test case classes and a test suites for transformations from
+ * <b>ASEM to SysML</b> models.
+ * 
+ * @author Benjamin Rupp
+ *
+ */
+package tools.vitruv.applications.asemsysml.tests.asem2sysml;
