bea9e8a64463bd5f0d105ca4d32075383182b70e
Benjamin Rupp <uwecl@student.kit.edu>
2017-02-08 17:50:22 +0100

    Add new project for ASEM to SysML reactions transformations

---
+++ bundles/asemsysml/tools.vitruv.applications.asemsysml.reactions.asem2sysml/src/tools/vitruv/applications/asemsysml/reactions/asem2sysml/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * Package for the reactions transformations from <b>ASEM to SysML</b>.
+ * 
+ * @author Benjamin Rupp
+ *
+ */
+package tools.vitruv.applications.asemsysml.reactions.asem2sysml;
