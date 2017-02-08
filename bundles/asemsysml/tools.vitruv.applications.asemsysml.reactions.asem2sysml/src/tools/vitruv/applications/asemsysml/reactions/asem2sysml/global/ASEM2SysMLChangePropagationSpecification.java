45ffe564267b803854b1f3d00d1563e0ad236b20
Benjamin Rupp <uwecl@student.kit.edu>
2017-02-08 18:01:25 +0100

    Add change propagation specification for reactions (ASEM2SysML)

---
+++ bundles/asemsysml/tools.vitruv.applications.asemsysml.reactions.asem2sysml/src/tools/vitruv/applications/asemsysml/reactions/asem2sysml/global/ASEM2SysMLChangePropagationSpecification.java
@@ -0,0 +1,13 @@
+package tools.vitruv.applications.asemsysml.reactions.asem2sysml.global;
+
+import mir.reactions.AbstractChangePropagationSpecificationEdu_kit_ipd_sdq_asemToUML;
+
+public class ASEM2SysMLChangePropagationSpecification
+        extends AbstractChangePropagationSpecificationEdu_kit_ipd_sdq_asemToUML {
+
+    @Override
+    protected void setup() {
+        super.setup();
+    }
+
+}
---
+++ bundles/asemsysml/tools.vitruv.applications.asemsysml.reactions.asem2sysml/src/tools/vitruv/applications/asemsysml/reactions/asem2sysml/global/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * This package will be exported by the ASEM2SysML plug-in and contains classes
+ * which can or should be used in the test cases, too.
+ * 
+ * @author Benjamin Rupp
+ *
+ */
+package tools.vitruv.applications.asemsysml.reactions.asem2sysml.global;
